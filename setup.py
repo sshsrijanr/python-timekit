3b50bdb37690e2f15d4ae55cc466c41fe5bba28e
srijan-ramavat <srijan.ramavat@gmail.com>
2020-09-20 20:08:44 +0530

    version update in setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='python-timekit',
-    version='0.0.1',
+    version='0.1.0',
     description='A package to integrate Timekit APIs with your project',
     py_modules=['timekit'],
     package_dir={'': 'src'},
